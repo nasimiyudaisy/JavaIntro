2e7826dec386dabae58705cfaab0e731759f56df
nasimiyudaisy <daicyamani12@gmail.com>
2017-09-23 13:33:14 +0300

    used loops

---
+++ src/Loop.java
@@ -0,0 +1,7 @@
+package src;
+
+/**
+ * Created by admin on 9/22/2017.
+ */
+public class Loop {
+}
---
+++ src/org/thecodevillage/intro/MyCollections.java
@@ -0,0 +1,7 @@
+package src.org.thecodevillage.intro;
+
+/**
+ * Created by admin on 9/23/2017.
+ */
+public class MyCollections {
+}
